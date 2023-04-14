da5b2341e8247d08828cd0a7f74a4a4d200933b4
Senpai <pvnt20@gmail.com>
2023-04-14 22:37:50 +0530

    Initial commit

---
+++ characters/character.py
@@ -0,0 +1,162 @@
+import re
+from selectolax.parser import HTMLParser
+from typing import List
+from sqlmodel import JSON, Column, SQLModel, Session, Field, create_engine, select
+from rich import print
+
+sqlite_url = "sqlite:///characters.db"
+engine = create_engine(sqlite_url, echo=False)
+
+
+class Character(SQLModel, table=True):
+    url: str
+    mal_id: int = Field(primary_key=True)
+    title: str
+    kanji_title: str = Field(nullable=True)
+    about: str = Field(nullable=True)
+    nicknames: List[str] = Field(sa_column=Column("nicknames", JSON), nullable=True)
+    favorite: int = Field(nullable=True)
+    image: str = Field(nullable=True)
+    animeography: List[str] = Field(
+        sa_column=Column("animeography", JSON), nullable=True
+    )
+    mangaography: List[str] = Field(
+        sa_column=Column("mangaography", JSON), nullable=True
+    )
+    voice_actors: List[dict] = Field(
+        sa_column=Column("voice_actors", JSON), nullable=False
+    )
+
+
+Character.metadata.create_all(engine)
+
+
+class CharacterParser:
+    def __init__(self, html: str):
+        self.html = html
+        self.parser = self.get_parser()
+
+    def get_parser(self):
+        return HTMLParser(self.html)
+
+    @staticmethod
+    def parse_malid(url: str):
+        return int(url.split("/")[-2])
+
+    def get_malid(self):
+        return self.parse_malid(
+            self.parser.css_first("meta[property='og:url']").attributes["content"]
+        )
+
+    def get_character_url(self):
+        return self.parser.css_first("meta[property='og:url']").attributes["content"]
+
+    def get_title(self):
+        return self.parser.css_first("meta[property='og:title']").attributes["content"]
+
+    def get_kanji_title(self):
+        try:
+            return (
+                self.parser.css_first("h2.normal_header span small").text().strip("()")
+            )
+        except Exception:
+            return ""
+
+    def get_about(self):
+        try:
+            text = self.parser.css_first("#content > table tr > td:nth-child(2)").text()
+            return re.search(r"<br><br(.*?)<br><br>", text)[0].strip("<br><br>")
+        except Exception:
+            return ""
+
+    def get_nicknames(self):
+        try:
+            aliases = self.parser.css_first("h1").text().strip()
+            aliases = aliases[aliases.find('"') + 1 : aliases.rfind('"')].strip()
+            return aliases.split(", ")
+        except Exception:
+            return []
+
+    def get_member_favorites_count(self):
+        try:
+            pattern = r"Member Favorites: (\d+,?\d+)"
+            match = re.search(pattern, self.html)
+            return re.search(r"\d+,?\d+", match[0])[0].replace(",", "")
+        except Exception:
+            return None
+
+    def get_image(self):
+        try:
+            return self.parser.css_first("meta[property='og:image']").attributes[
+                "content"
+            ]
+        except Exception:
+            return ""
+
+    def get_animeography(self):
+        try:
+            anime_links = {
+                link
+                for link in self.parser.css("td.borderClass tr a")
+                if "/anime/" in link.attributes.get("href", "")
+            }
+
+            return list(
+                {self.parse_malid(link.attributes["href"]) for link in anime_links}
+            )
+        except Exception:
+            return []
+
+    def get_mangagraphy(self):
+        try:
+            manga_links = [
+                link
+                for link in self.parser.css("td.borderClass tr a")
+                if "/manga/" in link.attributes.get("href", "")
+            ]
+            return list(
+                {self.parse_malid(link.attributes["href"]) for link in manga_links}
+            )
+        except Exception:
+            return []
+
+    def get_voice_actors(self) -> List[dict]:
+        try:
+            return [
+                {
+                    "language": td.css_first("small").text(),
+                    "mal_id": self.parse_malid(td.css_first("a").attributes["href"]),
+                }
+                for td in self.parser.css("table > tbody > tr > td:nth-child(2)")
+                if "/people/" in td.css_first("a").attributes["href"]
+            ]
+        except Exception:
+            return []
+
+    def get_character(self):
+        return Character(
+            url=self.get_character_url(),
+            about=self.get_about(),
+            title=self.get_title(),
+            mal_id=self.get_malid(),
+            kanji_title=self.get_kanji_title(),
+            nicknames=self.get_nicknames(),
+            favorite=self.get_member_favorites_count(),
+            image=self.get_image(),
+            animeography=self.get_animeography(),
+            mangaography=self.get_mangagraphy(),
+            voice_actors=self.get_voice_actors(),
+        )
+
+
+def insert_character(character: Character):
+    with Session(engine) as session:
+        session.add(character)
+        session.commit()
+        print("Added character: ", character.title)
+
+
+def exist_character(url: str):
+    with Session(engine) as session:
+        statement = select(Character).where(Character.url == url)
+        return bool(session.exec(statement).first())
---
+++ characters/characters.py
@@ -0,0 +1,78 @@
+from itertools import count
+import time
+import xmltodict
+from character import insert_character, CharacterParser, exist_character
+import asyncio
+import httpx
+from rich import print
+
+counter = count(1)
+exist_counter = count(1)
+REQUEST_LIMIT = 10
+TIMEOUT = 60
+headers = {
+    "authority": "myanimelist.net",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en",
+    "cache-control": "max-age=0",
+    "dnt": "1",
+    "referer": "https://myanimelist.net/",
+    "sec-ch-ua": "^^Chromium^^;v=^^112^^, ^^Google",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "^^Windows^^",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+}
+
+
+async def get_request(url: str, *args, **kwargs):
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url, *args, **kwargs)
+            print(next(counter), url)
+            response.raise_for_status()
+            return response
+        except (httpx.RequestError, httpx.HTTPStatusError):
+            print(f"Temporary banned! timeout {TIMEOUT} seconds")
+            time.sleep(TIMEOUT)  # block the async
+            raise Exception("4xx Error")
+
+
+async def get_characters_pages():
+    character_url1 = "https://myanimelist.net/sitemap/character-000.xml"
+    resp = await get_request(character_url1, headers=headers)
+    doc = xmltodict.parse(resp.text)
+    urls = list({key["loc"] for key in doc["urlset"]["url"]})
+    print(f"TOTAL URLS: {len(urls)}")
+    for i in range(0, len(urls), REQUEST_LIMIT):
+        yield urls[i : i + REQUEST_LIMIT]
+
+
+async def process_character(url: str):
+    try:
+        if exist_character(url):
+            print("Exist Count:", next(exist_counter), url)
+            return
+        resp = await get_request(url, headers=headers)
+        character_parser = CharacterParser(resp.text)
+        character = character_parser.get_character()
+        insert_character(character)
+    except Exception as e:
+        print("Failed", url)
+        print(e)
+
+
+async def main():
+    tasks = []
+    async for urls in get_characters_pages():
+        tasks.extend(asyncio.create_task(process_character(url)) for url in urls)
+        results = await asyncio.gather(*tasks)
+        print(f"Processed {len(results)} characters")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
