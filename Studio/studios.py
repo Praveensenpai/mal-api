d27d2f7fd19980b2061c76b872091eb9ce7f3658
Senpai <pvnt20@gmail.com>
2023-04-17 21:13:13 +0530

    added basic studio

---
+++ Studio/studios.py
@@ -0,0 +1,20 @@
+from selectolax.parser import HTMLParser
+import httpx
+
+
+def get_request(url) -> httpx.Response:
+    return httpx.get(url)
+
+
+def get_parser(url: str) -> HTMLParser:
+    resp = get_request(url)
+    return HTMLParser(resp.text)
+
+
+def get_studio_pages():
+    parser = get_parser("https://myanimelist.net/anime/producer")
+    return (
+        a.attributes["href"]
+        for a in parser.css("a.genre-name-link")
+        if "/producer/" in a.attributes["href"]
+    )
---
+++ Studio/test.py
@@ -0,0 +1,4 @@
+from studios import get_studio_pages
+
+for page in get_studio_pages():
+    print(page)
